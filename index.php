80bfd24b051d8647c75d60c6a03a2f6be0710e18
Zawatsuka <thomaslucas2904@gmail.com>
2021-01-17 19:31:20 +0100

    let's goooo

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="assets/img/logowhite_1.png" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
@@ -16,7 +17,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="text-center">
-                    <img src="assets\img\logonight.svg" class="img-fluid mt-3 " width="200" alt="logo lucas"
+                    <img src="assets/img/logonight.svg" class="img-fluid mt-3 " width="200" alt="logo lucas"
                         id="imgChange">
                     <a href="#" id="changeTheme"><img src="assets\img\lune.svg" id="moon" class="img-fluid mt-3"
                             width="70" alt="lune"></a>
