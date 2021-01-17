ad3c6103e329fc8415a281b943c2f09f782b9bdd
Zawatsuka <thomaslucas2904@gmail.com>
2021-01-17 18:37:25 +0100

    maj

--- template/portfolio.php
+++ template/portfolio.php
@@ -1,4 +1,5 @@
-<a class="btn btn-danger" data-toggle="collapse" href="#illu" role="button" aria-expanded="false"
+<!-- illustration  -->
+<a class="btn btn-danger btn-lg" data-toggle="collapse" href="#illu" role="button" aria-expanded="false"
     aria-controls="collapseExample">
     Illustration
 </a>
@@ -6,29 +7,84 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <img src="assets\img\portfolio\illu\chelou.jpg" class="img-fluid" width="155" alt="">
-                <img src="assets\img\portfolio\illu\chevalier.jpg" class="img-fluid" width="155" alt="">
-                <img src="assets\img\portfolio\illu\citrouille.jpg" class="img-fluid" width="155" alt="">
+                <a href="https://www.instagram.com/p/CANrjRcDVqv/?utm_source=ig_web_copy_link"><img
+                        src="assets\img\portfolio\illu\chelou.jpg" class="img-fluid zoom" width="300" alt=""></a>
+                <a href="https://www.instagram.com/p/CDrfA53jNnt/?utm_source=ig_web_copy_link"><img
+                        src="assets\img\portfolio\illu\chevalier.jpg" class="img-fluid zoom" width="300" alt=""></a>
+                <a href="https://www.instagram.com/p/CAvNh2uDNcX/?utm_source=ig_web_copy_link"><img
+                        src="assets\img\portfolio\illu\citrouille.jpg" class="img-fluid zoom" width="300" alt=""></a>
             </div>
             <div class="col-12">
-                <img src="assets\img\portfolio\illu\clownboy.jpg" class="img-fluid" width="155" alt="">
-                <img src="assets\img\portfolio\illu\demon.jpg" class="img-fluid" width="155" alt="">
-                <img src="assets\img\portfolio\illu\clown.jpg" class="img-fluid" width="155" alt="">
+                <a href="https://www.instagram.com/p/CA78u9BjLqH/?utm_source=ig_web_copy_link"><img
+                        src="assets\img\portfolio\illu\clownboy.jpg" class="img-fluid zoom" width="300" alt=""></a>
+                <a href="https://www.instagram.com/p/B9IQQ0gjfs-/?utm_source=ig_web_copy_link"><img
+                        src="assets\img\portfolio\illu\demon.jpg" class="img-fluid zoom" width="300" alt=""></a>
+                <a href="https://www.instagram.com/p/CBDBfkhDIT5/?utm_source=ig_web_copy_link"><img
+                        src="assets\img\portfolio\illu\clown.jpg" class="img-fluid zoom" width="300" alt=""></a>
             </div>
             <div class="col-12">
-                <img src="assets\img\portfolio\illu\goblin.png" class="img-fluid" width="155" alt="">
-                <img src="assets\img\portfolio\illu\mask.jpg" class="img-fluid" width="155" alt="">
-                <img src="assets\img\portfolio\illu\meuf.jpg" class="img-fluid" width="155" alt="">
+                <a href="https://www.instagram.com/p/CIaq4kDjkAL/?utm_source=ig_web_copy_link"><img
+                        src="assets\img\portfolio\illu\goblin.png" class="img-fluid zoom" width="300" alt=""></a>
+                <a href="https://www.instagram.com/p/CDBcF_jDLoh/?utm_source=ig_web_copy_link"><img
+                        src="assets\img\portfolio\illu\mask.jpg" class="img-fluid zoom" width="300" alt=""></a>
+                <a href="https://www.instagram.com/p/B5dC0GLgusy/?utm_source=ig_web_copy_link"><img
+                        src="assets\img\portfolio\illu\meuf.jpg" class="img-fluid zoom" width="300" alt=""></a>
             </div>
             <div class="col-12">
-                <img src="assets\img\portfolio\illu\space.jpg" class="img-fluid" width="155" alt="">
-                <img src="assets\img\portfolio\illu\spiderman.jpg" class="img-fluid" width="155" alt="">
-                <img src="assets\img\portfolio\illu\skul.jpg" class="img-fluid" width="155" alt="">
+                <a href="https://www.instagram.com/p/B-ZfliCjccj/?utm_source=ig_web_copy_link"><img
+                        src="assets\img\portfolio\illu\space.jpg" class="img-fluid zoom" width="300" alt=""></a>
+                <a href="https://www.instagram.com/p/CDjWFJDDRAm/?utm_source=ig_web_copy_link"><img
+                        src="assets\img\portfolio\illu\spiderman.jpg" class="img-fluid zoom" width="300" alt=""></a>
+                <a href="https://www.instagram.com/p/B_-spgtj-pX/?utm_source=ig_web_copy_link"><img
+                        src="assets\img\portfolio\illu\skul.jpg" class="img-fluid zoom" width="300" alt=""></a>
             </div>
             <div class="col-12">
-                <img src="assets\img\portfolio\illu\voleur.png" class="img-fluid" width="155" alt="">
-                <img src="assets\img\portfolio\illu\wow.png" class="img-fluid" width="155" alt="">
-            </div>  
+                <a href="https://www.instagram.com/p/B6X-IgLDxfi/?utm_source=ig_web_copy_link"><img
+                        src="assets\img\portfolio\illu\voleur.png" class="img-fluid zoom" width="300" alt=""></a>
+                <a href="https://www.instagram.com/p/CIId64xDOVS/?utm_source=ig_web_copy_link"><img
+                        src="assets\img\portfolio\illu\wow.png" class="img-fluid zoom" width="300" alt=""></a>
+            </div>
+            <div class="col-12">
+                <a href="https://www.instagram.com/zawa_tsuka29/">
+                    <p class="zoom"> vous voulez en voir plus n'hesitez pas a venir sur mon insta ! <img
+                            src="assets\img\insta dessin.png" class="img-fluid" width="100" alt=""></p>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<a class="btn btn-danger btn-lg" data-toggle="collapse" href="#site" role="button" aria-expanded="false"
+    aria-controls="collapseExample">
+    Sites
+</a>
+<div class="collapse" id="site">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <a href="https://zawatsuka.github.io/Le-loot/"><img src="assets/img/site/leloot.png"
+                        class="img-fluid zoom" width="300" alt="le loot site"></a>
+                        <p>(/!\site non responsive /!\) Pour un projet d'ecole on a creer un site pour un Bar orienté gaming nommer le "Le Loot".</p>
+                <a href="https://yanngosselin1986.github.io/Projet-vinyl/"><img src="assets/img/site/french-valine.png"
+                        class="img-fluid zoom" width="300" alt="projet vynile site"></a>
+                        <p>Pour un projet d'ecole nous avons du rentre un site deja creer responsive.</p>
+                <a href="https://github.com/TimothyJounier/Mood-Base"><img src="assets\img\site\moodbase.png" class="img-fluid zoom" width="300"
+                        alt="moodbase site"></a>
+                        <p>(lien gitHub)Pour un projet d'ecole,nous avons du creer un site e-commerce, le site avait pour but de vendre des socles en bois pour le high tech</p>
+            </div>
+        </div>
+    </div>
+</div>
+<a class="btn btn-danger btn-lg" data-toggle="collapse" href="#logo" role="button" aria-expanded="false"
+    aria-controls="collapseExample">
+    Logos
+</a>
+<div class="collapse" id="logo">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                    
+            </div>
         </div>
     </div>
 </div>
